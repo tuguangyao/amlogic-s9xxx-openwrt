a7911abf7034e0e7c579774872b7e8449a37072a
tuguangyao <tuhaiyang@hotmail.com>
2025-12-05 11:15:44 +0800

    Update diy-part1.sh

--- config/immortalwrt_master/diy-part1.sh
+++ config/immortalwrt_master/diy-part1.sh
@@ -3,9 +3,9 @@ cd openwrt
 
 
 # Add luci-app-amlogic
-git clone https://github.com/ophub/luci-app-amlogic.git   package-temp/luci-app-amlogic
-mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/
-rm -rf package-temp
+#git clone https://github.com/ophub/luci-app-amlogic.git   package-temp/luci-app-amlogic
+#mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/
+#rm -rf package-temp
 # Add luci-app-openclash
 ## 下载OpenClash
 wget https://github.com/vernesong/OpenClash/archive/master.zip
