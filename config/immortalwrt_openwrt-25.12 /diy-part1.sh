e6587e7790f4a83d5b921d0386e71d7f103fffd5
tuguangyao <tuhaiyang@hotmail.com>
2026-01-01 12:59:34 +0800

    Update diy-part1.sh

--- config/immortalwrt_openwrt-25.12 /diy-part1.sh
+++ config/immortalwrt_openwrt-25.12 /diy-part1.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+# Add luci-app-amlogic
+#git clone https://github.com/ophub/luci-app-amlogic.git   package-temp/luci-app-amlogic
+#mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/
+#rm -rf package-temp
+# Add luci-app-passwall
+sed -i '/src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main/d' feeds.conf.default
+sed -i '/src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main/d' feeds.conf.default
+# Add luci-app-openclash
+## 下载OpenClash
+wget https://github.com/vernesong/OpenClash/archive/master.zip
+## 解压
+unzip master.zip
+cp -r OpenClash-master/luci-app-openclash package/
